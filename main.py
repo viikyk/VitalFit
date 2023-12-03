036a0d80308cdb4006fe3f459a091363468548cd
samyrafariass <104604760+samyrafariass@users.noreply.github.com>
2023-12-03 18:10:28 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+# BIBLIOTECAS/IMPORTS
+
+# LISTAS/DICIONÁRIOS
+alunos = []
+questionario = []
+treino = []
+perfil_nutricional = []
+avaliacao_periodica = []
+produtos = []
+lista_desejos = []
+estoque = []
+
+# MENU
+def menu():
+  print()
+  print(' ..:: BEM VINDO(A) A ACADEMIA VITALFIT ::.. ')
+  print()
+  print('''Escolha uma opção:
+  [ 1 ] - Cadastro e Acompanhamento Individual
+  [ 2 ] - Moda Fitness VitalFit
+  [ 3 ] - Central de Treinamento Individual
+  [ 4 ] - Avaliação Nutricional
+  [ 5 ] - ENCERRAR PROGRAMA''')
+
+# MÓDULO 1
+def menu_mod1():
+  print('''Escolha uma opção:
+  1 - Cadastro de Alunos
+  2 - Avaliação Periódica
+  3 - Histórico de Acompanhamento
+  4 - Alertas Automáticos
+  5 - Comunicação Integrada
+  6 - Relatório de Progresso
+  7 - Personalização de Treino
+  8 - Buscar informações nutricionais
+  9 - SAIR/VOLTAR''')
+
+# MÓDULO 2
+def menu_mod2():
+  print('''Escolha uma opção:
+  1 - Cadastrar Produto
+  2 - Perfil de Compra do Aluno
+  3 - Promoções Personalizadas
+  4 - Lista de Desejos
+  5 - Avisos de Novos Produtos
+  6 - Sistema de Pagamento
+  7 - Acompanhamento de Entregas
+  8 - Programa de Fidelidade
+  9 - Avaliação de Produtos
+  10 - Gestão de Estoque
+  11 - SAIR/VOLTAR''')
+
+# MÓDULO 3
+def menu_mod3():
+  print('''Escolha uma opção:
+  1 - Montar Treino Personalizado
+  2 - Acesso a Biblioteca de Vídeos de Exercícios
+  3 - Rotinas de Treino Personalizadas
+  4 - Acompanhamento de desempenho
+  5 - Alerta de Treino
+  6 - Comunicação com o Professor
+  7 - Premiassões e Reconhecimentos
+  8 - SAIR/VOLTAR''')
+
+# MÓDULO 4
+def menu_mod4():
+  print('''Escolha uma opção:
+  1 - Perfil Nutricional Individual
+  2 - Questionário Individual
+  3 - Registro de Dieta Diário
+  4 - Orientação Nutricional Personalizada
+  5 - Acompanhamento do Consumo de Água e Suplementos
+  6 - Alertas de Metas Nutricionais
+  7 - Rastreamento Alimentar
+  8 - Comunicação com o Nutricionista
+  9 - SAIR/VOLTAR''')
+
+# CHAMADA DAS FUNÇÕES
+'''
+while True:
+  menu()
+  opcao = int(input('Opção: '))
+  if opcao == 1:
+    print()
+  elif opcao == 2:
+    print()
+  elif opcao == 3:
+    print()
+  elif opcao == 4:
+    print()
+  elif opcao == 5:
+    print('Encerrando o programa...')
+    break
+  else:
+    print('OPÇÃO INVÁLIDA! Tente novamente mais tarde.')
+
+'''
